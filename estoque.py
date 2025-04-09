c617cb59f858ee6d8ed82288e0a9fede469c40c6
david627462 <davidcardosobragantim@gmail.com>
2025-04-09 18:34:26 -0300

    codigo do lucas

--- estoque.py
+++ estoque.py
@@ -17,3 +17,26 @@ def subtrair(id:int, quant: int):
         return {False} 
 
      
+@app.put("/adicionar_estoque")
+def adicionar_estoque(id: int, qnt: int): 
+    try:
+        con = sqlite3.connect("/Users/POSITIVO/hackers/database/mercado.db")
+        cur = con.cursor()
+        cur.execute(""" update produtos set quantidade = ? where id = ? """, (qnt, id))
+        con.commit()
+        con.close()
+        return 1
+    except:
+        return -1
+
+@app.put("/reajustar_preco_total")
+def reajustar_preço_total(p: float):
+    try: 
+        con = sqlite3.connect("/Users/POSITIVO/hackers/database/mercado.db")
+        cur = con.cursor()
+        cur.execute(""" update produtos set preco = preco + (preco / 100 * ?) """, (p,))
+        con.commit()
+        con.close()
+        return 1
+    except:
+        return -1
