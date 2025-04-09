775695cb249303a0520c69174dc0d834ed31110b
David Cardoso <davidcardosobragantim@gmail.com>
2025-04-09 19:30:42 -0300

    codigo nathan

--- estoque.py
+++ estoque.py
@@ -40,3 +40,42 @@ def reajustar_preço_total(p: float):
         return 1
     except:
         return -1
+
+@app.post("/incluir_produto")
+def adicionar_produto(codigo: str,nome: str,quantidade: int,preco: float):
+    try: 
+        con = sqlite3.connect("/home/ijovem02/hackers/database/mercado.db")
+        cur = con.cursor()
+        cur.execute(""" insert into produtos ( codigo, nome, quantidade, preco) values (?,?,?,?) """ ,(codigo , nome , quantidade, preco,) )
+        con.commit()
+        con.close
+        return {True}
+    except:
+        return False      
+@app.get("/consultar_estoque")
+def revisar_estoque():
+
+    try:
+        con = sqlite3.connect("/home/ijovem02/hackers/database/mercado.db")
+        cur = con.cursor()
+        cur.execute(" select id, codigo, nome, quantidade from produtos  "  )
+        rows=cur.fetchall()
+        con.close
+        return {rows}
+    except:
+        return False
+
+    
+@app.put("/alterar_produto")
+def alterar_produto(id: int,codigo: str, nome:str):
+    try:
+        con = sqlite3.connect("/home/ijovem02/hackers/database/mercado.db")
+        cur = con.cursor()
+        cur.execute(""" update  produtos set codigo=?, nome=? where id=?  """ ,   (codigo, nome, id ) ) 
+        con.commit()
+        con.close
+        return {True}
+    except:
+        return False
+
+
